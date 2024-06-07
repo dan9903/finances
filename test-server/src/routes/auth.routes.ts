44846e1a967276286f1e65efaeaf98bb53ef9e1b
Damián Meneses <mnssd2@gmail.com>
2024-06-07 01:41:31 -0300

    feat: add return error, add cors support

--- test-server/src/routes/auth.routes.ts
+++ test-server/src/routes/auth.routes.ts
@@ -2,12 +2,22 @@ import { Router } from "express";
 
 const authRouter = Router();
 
-authRouter.post('/login', (req, res) => {
+authRouter.post('/sign-in', (req, res) => {
   console.log(req.body);
+  const { username } = req.body;
+  if (username === 'error') {
+    res.status(401)
+      .send({
+        error: {
+          message: "login error, please verify your credentials"
+        }
+      });
+  }
+
   res.statusCode = 200;
   res.json({
     username: "daisyta",
-    token: "authTokenFalso",
+    token: "authTokenFalse",
     roles: [
       "admin",
       "owner",
@@ -19,13 +29,43 @@ authRouter.post('/login', (req, res) => {
 
 });
 
-authRouter.post('/register', (req, res) => {
-  const [username, password, email] = req.body;
-
+authRouter.post('/sign-up', (req, res) => {
+  console.log(req.body);
+  res.statusCode = 201;
+  res.json({
+    username: "daisyta",
+    token: "authTokenFalse",
+    roles: [
+      "admin",
+      "owner",
+      "ruler",
+    ]
+  });
 
   return res;
 });
 
+authRouter.put('/forgot-password', (req, res) => {
+  console.log(req.body);
+  res.statusCode = 200;
+  return res;
+});
 
+authRouter.put('/renew-token', (req, res) => {
+  console.log(req.body);
+  res.statusCode = 200;
+  res.json({
+    token: "newFakeToken"
+  });
+
+  return res
+});
+
+authRouter.delete('/logout', (req, res) => {
+  console.log(req.body);
+  res.statusCode = 200;
+
+  return res;
+});
 
 export default authRouter;
--- test-server/src/server.ts
+++ test-server/src/server.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import authRouter from "./routes/auth.routes";
+import cors from "cors";
 
 const app = express();
 
+const options: cors.CorsOptions = {
+  origin: ['http://localhost:4200']
+}
+
+
+app.use(cors(options));
 app.use(express.json());
-app.use('/auth', authRouter);
+app.use('/api/auth', authRouter);
 
 const PORT = process.env.PORT || 8081;
 
