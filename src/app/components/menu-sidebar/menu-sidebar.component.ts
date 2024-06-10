b244a19ced05eba8024b4b9dd5cbe1959ff545ac
Damián Meneses <mnssd2@gmail.com>
2024-06-10 13:40:41 -0300

    feat: create menu sidebar component add to toolbar

---
+++ src/app/components/menu-sidebar/menu-sidebar.component.ts
@@ -0,0 +1,51 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { Sidebar } from 'primeng/sidebar';
+import { THEMES } from 'src/app/constants/themes';
+import ITheme from 'src/app/interfaces/ITheme';
+import { ThemeService } from 'src/app/services/misc/theme.service';
+
+@Component({
+  selector: 'app-menu-sidebar',
+  templateUrl: './menu-sidebar.component.html',
+  styleUrl: './menu-sidebar.component.scss'
+})
+export class MenuSidebarComponent implements OnChanges {
+  @Input({ required: true })
+  sidebarVisible: boolean = false;
+
+  @Output()
+  sidebarVisibleChange = new EventEmitter<boolean>();
+
+  themes = THEMES;
+  selectedTheme!: ITheme;
+
+
+  @ViewChild('sidebarRef') sidebarRef!: Sidebar;
+
+  closeCallback(e: MouseEvent): void {
+    this.sidebarRef.close(e);
+  }
+
+  constructor(private themeService: ThemeService) { }
+
+  ngOnInit() {
+    const themeId = this.themeService.getCurrentTheme();
+    const theme = THEMES.find((item: ITheme) => item.id === themeId);
+    this.selectedTheme = theme ? theme : THEMES[0];
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['sidebarVisible']?.currentValue) {
+      this.sidebarVisible = changes['sidebarVisible']?.currentValue;
+    }
+  }
+
+  sidebarChangeValue(value: boolean): void {
+    this.sidebarVisible = value;
+    this.sidebarVisibleChange.emit(value);
+  }
+
+  changeTheme(id: string) {
+    this.themeService.switchTheme(id);
+  }
+}
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { THEMES } from 'src/app/constants/themes';
-import ITheme from 'src/app/interfaces/ITheme';
-import { ThemeService } from 'src/app/services/misc/theme.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,26 +7,15 @@ import { ThemeService } from 'src/app/services/misc/theme.service';
 })
 export class ToolbarComponent implements OnInit {
   rangeDates: Date[] = [];
-  themes = THEMES;
-  selectedTheme!: ITheme;
+  sidebarMenuVisible = true;
 
-
-  constructor(private themeService: ThemeService, private router: Router) { }
+  constructor(private router: Router) { }
 
 
   ngOnInit(): void {
-    const themeId = this.themeService.getCurrentTheme();
-    const theme = THEMES.find((item: ITheme) => item.id === themeId);
-    this.selectedTheme = theme ? theme : THEMES[0];
-
-
     this.rangeDates.push(new Date());
     this.rangeDates.push(new Date());
     this.rangeDates[0].setMonth(this.rangeDates[0].getMonth() - 1);
-  }
-
-  changeTheme(id: string) {
-    this.themeService.switchTheme(id);
   }
 
   redirectTo(path: string): void {
@@ -38,5 +24,9 @@ export class ToolbarComponent implements OnInit {
 
   active(path: string): boolean {
     return this.router.isActive(path, true);
+  }
+
+  toggleSidebar(): void {
+    this.sidebarMenuVisible = !this.sidebarMenuVisible;
   }
 }
